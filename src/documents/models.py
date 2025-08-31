2c31d6d07c9f14d07ee308a285f51e80da205bc3
prabhsuratsingh <prabhsurat17@gmail.com>
2025-08-31 21:53:33 +0530

    update document model

--- src/documents/models.py
+++ src/documents/models.py
@@ -16,6 +16,9 @@ class Document(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"<Document: {self.title}>"
+
     def save(self, *args, **kwargs):
         if self.active and self.active_at is None:
             self.active_at = timezone.now()
